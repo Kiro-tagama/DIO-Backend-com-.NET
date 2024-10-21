e677713062e9f129daa2ce6e66b3dc3f7b3adc81
Rodrigo Lopes <59925590+Kiro-tagama@users.noreply.github.com>
2024-10-21 19:43:22 +0000

    start ASPNET minimals api

---
+++ project-minimal-api/Program.cs
@@ -0,0 +1,18 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/login", (LoginDTO loginDTO) =>{
+  var userAdmin = loginDTO.Email == "adm@test.com" && loginDTO.Password == "123456";
+
+  return userAdmin? Results.Ok("Login admin") : Results.Unauthorized(); 
+});
+
+app.Run();
+
+
+public class LoginDTO{
+  public string Email { get; set; } = default!;
+  public string Password { get; set; } = default!;
+}
