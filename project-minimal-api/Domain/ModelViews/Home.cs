5a2a557ef6f0fdfb8edca2262e63514017778a6a
Rodrigo Lopes <59925590+Kiro-tagama@users.noreply.github.com>
2024-10-24 14:30:55 +0000

    up

---
+++ project-minimal-api/Domain/ModelViews/Home.cs
@@ -0,0 +1,5 @@
+namespace MinimalApi.Domain.ModelViews;
+
+public struct Home{
+  public string Documentation {get=> "/swagger";}
+}
--- project-minimal-api/Program.cs
+++ project-minimal-api/Program.cs
@@ -2,12 +2,18 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MinimalApi.Domain.Interface;
 using MinimalApi.Domain.Services;
+using MinimalApi.Domain.ModelViews;
 using MinimalApi.DTOs;
 using MinimalApi.Infrastructure.Db;
 
+#region Builder
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddScoped<IAdministratorServices, AdministratorServices>();
+builder.Services.AddScoped<IVehicleServices, VehicleServices>();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<DbConnect>(options=>{
   options.UseMySql(
@@ -17,12 +23,19 @@ builder.Services.AddDbContext<DbConnect>(options=>{
 });
 
 var app = builder.Build();
+#endregion
 
+#region Home
+//app.MapGet("/",()=> Results.Json(new Home()));
 app.MapGet("/", async (DbConnect dbConnect) => {
   try{
     var canConnect = await dbConnect.Database.CanConnectAsync();
     return canConnect 
-      ? Results.Ok(new { message = "Servidor on", detail = "Conexão com MySQL bem-sucedida." }) 
+      ? Results.Ok(new { 
+        message = "Servidor on", 
+        detail = "Conexão com MySQL bem-sucedida.", 
+        doc = new Home()
+      }) 
       : Results.Json(new { message = "Falha ao conectar ao MySQL." }, statusCode: 500);
   }
   catch (Exception ex){
@@ -48,5 +61,34 @@ app.MapPost("/login",
     return Results.Json(new { message = "Erro", detail = ex.Message }, statusCode: 500);
   }
 });
+#endregion
+
+#region Vehicle
+  app.MapPost("/vehicles", 
+( 
+  [FromBody] 
+  VehicleDTO vehicleDTO,
+  IVehicleServices vehicleServices
+) =>{
+  try{
+    // var vehicle = new Vehicle{
+    //   Name = VehicleDTO.Name,
+    //   Mark = VehicleDTO.Mark,
+    //   Year = VehicleDTO.Year
+    // };
+
+    // vehicleServices.AddVehicle();
+
+    // return Results.Created($"/vehicle/{vehicle.Id}", vehicle);
+  }
+  catch (System.Exception ex)
+  {
+    return Results.Json(new { message = "Erro", detail = ex.Message }, statusCode: 500);
+  }
+});
+#endregion
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.Run();
